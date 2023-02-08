cf9d42ed53ef7cd76241befe03549fa27ae6a9da
Mika <michaeladafne@hotmail.com>
2023-02-08 10:46:40 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+          <img src="/mikarrega.png" className="logo" alt="Vite logo" />
+      </div>
+      <span>Site em construção</span>
+      <div className="card">
+      <a href="https://github.com/mikarrega" target="_blank">
+        <button>
+          Github
+        </button>
+      </a>
+      <a href="https://www.linkedin.com/in/michaeladafne/" target="_blank">
+        <button>
+          Linkedin
+        </button>
+      </a>
+      </div>
+    </div>
+  )
+}
+
+export default App
